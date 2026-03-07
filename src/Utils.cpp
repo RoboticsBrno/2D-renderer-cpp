9bbee823a40bf84f409bb10857c4c6955e56ecb0
JohnieWalkerCZ <jan.prochazka.cr@gmail.com>
2026-03-07 16:42:57 +0100

    Added != operator for Color struct

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -5,6 +5,8 @@ bool operator==(const Color &lhs, const Color &rhs) {
     return lhs.r == rhs.r && lhs.g == rhs.g && lhs.b == rhs.b && lhs.a == rhs.a;
 }
 
+bool operator!=(const Color &lhs, const Color &rhs) { return !(lhs == rhs); }
+
 bool operator==(const Pixel &lhs, const Pixel &rhs) {
     return lhs.x == rhs.x && lhs.y == rhs.y && lhs.color == rhs.color;
 }
